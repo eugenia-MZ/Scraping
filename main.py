f5fbda895db463b741e65ac633b6fa41a55254de
mzitouni <mzitouni@eugeniaschool.com>
2025-10-10 12:23:10 +0000

    bookstoscrape test

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
 import requests
+from bs4 import BeautifulSoup
 
-response = requests.get("https://quotes.toscrape.com/")
+response = requests.get("https://books.toscrape.com/")
 
-print(response.status_code)
-print(response.text)
+content_html = response.text
+
+soup = BeautifulSoup(content_html, "html.parser")
+
+print(soup.title.get_text(strip=True))
+
+resultats = soup.find_all("strong")
+print(resultats)
+
+prix = [float(p.get_text()[2:]) for p in soup.find_all("p", class_="price_color")]
+
+print(f"Voici les prix : {prix}")
+
+somme = sum(prix)
+print(somme)
+moyenne = somme / len(prix)
+print(f"Voici la moyenne des prix : {moyenne:.2f}£")
+
+# with open("test.html", "w") as file:
+#    file.write(content_html)
